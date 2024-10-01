6694055e22846de562cbc53fffd70cdf43d3786d
Olga <o-lin98@mail.ru>
2024-10-01 12:22:56 +0300

    Cards have been added to the page. The ability to delete cards with a mouse click has been implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+function addCard(){
+    document.querySelector('.places__list').append(cardElement);
+}
 
-// @todo: DOM узлы
+for (let i=0; i<initialCards.length; i++){
+    cardTemplate = document.querySelector('#card-template').content;
+    cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = initialCards[i].link;
+    cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+    addCard()
+}
 
-// @todo: Функция создания карточки
+delElement = document.querySelectorAll('.card__delete-button');
+arrDelElement = Array.from(delElement);
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for (let i=0; i<arrDelElement.length; i++){
+    arrDelElement[i].addEventListener('click', () => {
+    const listItem = arrDelElement[i].closest('.places__item');
+    listItem.remove(); 
+  })
+}
